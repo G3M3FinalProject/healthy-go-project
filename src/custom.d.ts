40fac55ecc1f275c8d3a2b0e77bc2700e8fa3653
Amanda <amandard94@gmail.com>
2022-08-27 11:38:39 -0300

    feat: modules declaration for images extentions

---
+++ src/custom.d.ts
@@ -0,0 +1,5 @@
+declare module 'styled-components';
+declare module '*.jpg';
+declare module '*.png';
+declare module '*.jpeg';
+declare module '*.gif';
--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,1 +1,33 @@
-:root 
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+:root {
+    --brand-green: rgba(36, 133, 64, 1);
+    --brand-yellow: rgba(227, 229, 34, 1);
+
+    * { 
+        margin:0;
+        padding: 0;
+        outline:0;
+        box-sizing: border-box;
+
+        -webkit-font-smoothing: antialiased;
+        overflow-y: auto;
+        width: 100vw;
+        height: 100vh;
+    }
+
+    button {
+        cursor: pointer;
+        border: 0;
+    }
+
+    input {
+        border: 0;
+    }
+
+    ul {
+        list-style: none;
+    }
+}
+`;
