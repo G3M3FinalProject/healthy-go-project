dd0180d6859c71bbe1151a3794098a5ce1a4e8c4
Amanda <amandard94@gmail.com>
2022-08-29 19:31:19 -0300

    feat: hamburguer menu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,34 @@
 import { motion } from "framer-motion";
 
 import flag from "../../assets/flag.png";
+import HealthyGo from "../../assets/healthygo.png";
 import LogoImg from "../../assets/logo.png";
-import { Container, Flag, Logo, Brand, Menu } from "./styles";
+import mobileflag from "../../assets/mobileflag.png";
+import {
+  Container,
+  Flag,
+  Source,
+  Logo,
+  BrandName,
+  Brand,
+  Menu,
+  HamburguerMenu,
+  HamburguerLine,
+  Paragraph,
+} from "./styles";
 
 const Header = () => {
+  // const [isHamburguer, setHamburguer] = useState(window.innerWidth < 425);
+
+  // const updateMedia = () => {
+  //   setDesktop(window.innerWidth < 425);
+  // };
+
+  // useEffect(() => {
+  //   window.addEventListener("resize", updateMedia);
+  //   return () => window.removeEventListener("resize", updateMedia);
+  // });
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -12,15 +36,26 @@ const Header = () => {
       exit={{ opacity: 0 }}
       transition={{ duration: 1 }}
     >
-      <Flag src={flag} alt="flag" />
+      <picture>
+        <Source media="(max-width: 425px)" srcSet={mobileflag} />
+        <Source media="(min-width: 426px)" srcSet={flag} />
+        <Flag src={flag} alt="flag" />
+      </picture>
       <Container>
         <Brand>
           <Logo src={LogoImg} alt="Logo" />
-          <h1>Healthy Go</h1>
+          <BrandName src={HealthyGo} alt="brand name" />
         </Brand>
         <Menu>
-          <p>Login</p>
-          <p>Cadastro</p>
+          <HamburguerMenu>
+            <HamburguerLine className="line-1" />
+            <HamburguerLine className="line-2" />
+            <HamburguerLine className="line-3" />
+          </HamburguerMenu>
+          <Paragraph>
+            <p>Login</p>
+            <p>Cadastro</p>
+          </Paragraph>
         </Menu>
       </Container>
     </motion.div>
