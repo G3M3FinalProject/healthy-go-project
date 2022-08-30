4947c25235b8a9c5bc67441972fda434c8ce896c
Amanda <amandard94@gmail.com>
2022-08-30 15:57:52 -0300

    removed searchbar component line from .app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { SeachBar } from "./components/global-inputs";
 import Header from "./components/header";
 import AppRoutes from "./routes";
 import GlobalStyle from "./styles/GlobalStyles";
