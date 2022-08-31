886804692105a4ebad656d50e0434591a5179314
Queren Hope <querenhope@hotmail.com>
2022-08-31 15:14:58 -0300

    add: estilizaçao carrinho

--- src/components/cart/index.tsx
+++ src/components/cart/index.tsx
@@ -1,5 +1,6 @@
 import { AiOutlineLeft } from "react-icons/ai";
 import { FaTrashAlt } from "react-icons/fa";
+import { MdAdd } from "react-icons/md";
 
 import { Container, Modal } from "./styles";
 
@@ -10,86 +11,115 @@ const Cart = () => {
     <Modal>
       <Container>
         <div className="header-cart">
-          <button>
+          <button className="back-cart">
             <AiOutlineLeft />
           </button>
           <h3>Carrinho</h3>
         </div>
-        <div className="desconto">
-          <p>
-            Gaste mais R$ 50,00 e ganhe <strong>10% de desconto</strong>{" "}
-          </p>
+        <div className="container-itens">
+          <div className="desconto">
+            <p>
+              Gaste mais R$ 50,00 e ganhe <strong>10% de desconto</strong>{" "}
+            </p>
+          </div>
+          <div className="cart-restaurantes">
+            <h2>Restaurante da Mamae</h2>
+            <button className="retornar">Retornar para a Loja </button>
+            <div className="card-item">
+              <div className="item">
+                <img src="../../assets/prato.png" alt="" />
+                <div className="info">
+                  <p>X-Burguer</p>
+                  <strong>R$ 10,00</strong>
+                </div>
+                <div className="quantidade">
+                  <button>
+                    <FaTrashAlt />
+                  </button>
+                  <p>1</p>
+                  <button>
+                    <MdAdd />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="card-item">
+              <div className="item">
+                <img src="../../assets/prato.png" alt="" />
+                <div>
+                  <p>X-Burguer</p>
+                  <strong>R$ 10,00</strong>
+                </div>
+                <div className="quantidade">
+                  <button>
+                    <FaTrashAlt />
+                  </button>
+                  <p>1</p>
+                  <button>
+                    <MdAdd />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="cart-restaurantes">
+            <h2>Restaurante da Mamae</h2>
+            <button className="retornar">Retornar para a Loja </button>
+            <div className="card-item">
+              <div className="item">
+                <img src="../../assets/prato.png" alt="" />
+                <div className="info">
+                  <p>X-Burguer</p>
+                  <strong>R$ 10,00</strong>
+                </div>
+                <div className="quantidade">
+                  <button>
+                    <FaTrashAlt />
+                  </button>
+                  <p>1</p>
+                  <button>
+                    <MdAdd />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="card-item">
+              <div className="item">
+                <img src="../../assets/prato.png" alt="" />
+                <div>
+                  <p>X-Burguer</p>
+                  <strong>R$ 10,00</strong>
+                </div>
+                <div className="quantidade">
+                  <button>
+                    <FaTrashAlt />
+                  </button>
+                  <p>1</p>
+                  <button>
+                    <MdAdd />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="cart-restaurantes">
-          <h2>Restaurante da Mamae</h2>
-          <button>Retornar para a Loja </button>
-          <div className="card-item">
-            <div>
-              <img src="../../assets/prato.png" alt="" />
-              <div>
-                <p>X-Burguer</p>
-                <strong>R$ 10,00</strong>
-              </div>
-              <div>
-                <button>
-                  <FaTrashAlt />
-                </button>
-                <p>1</p>
-                <button>+</button>
-              </div>
+        <div className="rodape-cart">
+          <div className="info-total">
+            <div className="subtotal">
+              <p>Subtotal</p>
+              <p>R$ 105,00</p>
+            </div>
+            <div className="frete">
+              <p>Frete</p>
+              <p>R$ 20,00</p>
             </div>
           </div>
-          <div className="card-item">
-            <div>
-              <img src="../../assets/prato.png" alt="" />
-              <div>
-                <p>X-Burguer</p>
-                <strong>R$ 10,00</strong>
-              </div>
-              <div>
-                <button>
-                  <FaTrashAlt />
-                </button>
-                <p>1</p>
-                <button>+</button>
-              </div>
+          <div className="finalizar-cart">
+            <div className="total">
+              <p>Total</p>
+              <strong className="soma"> R$ 100,00</strong>
             </div>
-          </div>
-        </div>
-        <div className="cart-restaurantes">
-          <h2>Restaurante da Mamae</h2>
-          <button>Retornar para a Loja </button>
-          <div className="card-item">
-            <div>
-              <img src="../../assets/prato.png" alt="" />
-              <div>
-                <p>X-Burguer</p>
-                <strong>R$ 10,00</strong>
-              </div>
-              <div>
-                <button>
-                  <FaTrashAlt />
-                </button>
-                <p>1</p>
-                <button>+</button>
-              </div>
-            </div>
-          </div>
-          <div className="card-item">
-            <div>
-              <img src="../../assets/prato.png" alt="" />
-              <div>
-                <p>X-Burguer</p>
-                <strong>R$ 10,00</strong>
-              </div>
-              <div>
-                <button>
-                  <FaTrashAlt />
-                </button>
-                <p>1</p>
-                <button>+</button>
-              </div>
-            </div>
+            <button>Finalizar Pedido</button>
           </div>
         </div>
       </Container>
