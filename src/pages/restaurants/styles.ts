5cc6ff1eb18f16626aa27fd5118e4af74d2a23f9
Carol Pezzin <102972588+CarolPezzin@users.noreply.github.com>
2022-09-02 13:30:54 -0300

    alterações feitas apenas no CSS

--- src/pages/restaurants/styles.ts
+++ src/pages/restaurants/styles.ts
@@ -1,40 +1,72 @@
 import styled from "styled-components";
 
+export const ContainerRestaurants = styled.div`
+  width: 80vw;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: auto;
+`;
+
 export const ContainerTop = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
 
-  width: 50vw;
-  margin-left: 5%;
+  width: 40vw;
+
+  @media screen and (max-width: 765px) {
+    width: 100%;
+    margin-right: 3%;
+    font-size: 8px;
+  }
+  @media screen and (max-width: 425px) {
+    width: 100%;
+    margin-right: 3%;
+    font-size: 8px;
+    img {
+      display: none;
+    }
+  }
 `;
 
 export const ContainerHeader = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  width: 90vw;
+
+  width: 100%;
+
   margin-top: 1%;
   margin-left: auto;
   margin-right: auto;
+
   border-top: 1px solid;
   border-left: 1px solid;
   border-right: 1px solid;
   border-radius: 16px 16px 0 0;
   border-color: var(--brand-light-green);
+
   div {
     margin-left: 1%;
     margin-left: 2rem;
     h3 {
       font-family: "Courgette";
       font-weight: 400;
-      font-size: 54px;
+      font-size: 48px;
+
+      @media screen and (max-width: 765px) {
+        font-size: 36px;
+      }
+
+      @media screen and (max-width: 425px) {
+        font-size: 36px;
+      }
     }
     p {
       font-family: "Petrona";
       font-style: normal;
       font-weight: 400;
-      font-size: 16px;
+      font-size: 12px;
     }
   }
   figure {
