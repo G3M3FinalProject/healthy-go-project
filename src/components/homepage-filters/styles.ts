9ea2aef2217f22aed99525ae4102d826313a1529
Amanda <amandard94@gmail.com>
2022-09-02 23:23:15 -0300

    feat: seach input and filters styled

--- src/components/homepage-filters/styles.ts
+++ src/components/homepage-filters/styles.ts
@@ -1,6 +1,37 @@
 import styled from "styled-components";
 
-export const ContainerFilters = styled.div`
-  height: 150px;
-  grid-area: filters;
+export const PositioningDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 0.5rem;
+
+  button {
+    width: 6rem;
+    height: 2rem;
+
+    border-radius: 20px;
+
+    font-size: 15px;
+    font-weight: 500;
+    font-family: "Petrona", serif;
+
+    background: var(--brand-light-green);
+
+    cursor: pointer;
+
+    &:hover {
+      color: white;
+      background: var(--button-green);
+    }
+  }
 `;
