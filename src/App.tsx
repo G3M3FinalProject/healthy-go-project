e14edec3c0c76231a9a89d95b4ebf1f474906a2e
Amanda <93018313+ahmandi@users.noreply.github.com>
2022-09-07 22:33:18 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import Header from "./components/header";
 import { ToasterContainer } from "./components/toaster-container";
 import { Providers } from "./contexts";
+import { AppThemeProvider } from "./contexts/themeProvider";
 import AppRoutes from "./routes";
 import GlobalStyle from "./styles/GlobalStyles";
 
 function App() {
   return (
-    <Providers>
-      <GlobalStyle />
-      <Header />
-      <AppRoutes />
-      <ToasterContainer />
-    </Providers>
+    <AppThemeProvider>
+      <Providers>
+        <GlobalStyle />
+        <Header />
+        <AppRoutes />
+        <ToasterContainer />
+      </Providers>
+    </AppThemeProvider>
   );
 }
 
